c122872eca8c67b76e4fd3c4d187fe9030edcb65
Sourav Bhowal <souravbhowal15@gmail.com>
2024-12-24 22:24:24 +0530

    chore: Add frontend files and dependencies for React + Vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,4 @@
+// App component is the root component of the application. It is the parent component of all other components. It is the first component that gets rendered when the application starts.
+export default function App() {
+  return <div>app</div>;
+}
